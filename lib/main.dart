b9fd0c925f99e26edcd5df90de8883544730adb6
omar <omarmakram421@gmail.com>
2023-08-19 22:23:51 +0300

    add hadith_details

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:islamic/core/my_theme/my_theme.dart';
 import 'package:islamic/modules/splash_screen/splash_screen.dart';
 
 import 'layout/home/home_screen.dart';
+import 'modules/hadeth_view/hadith_details/hadith_details.dart';
 import 'modules/quran_view/sura_details/sura_details.dart';
 
 void main() {
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
         SplashScreen.routeName: (context) => SplashScreen(),
         HomeScreen.routeName: (context) => HomeScreen(),
         SuraDetails.routeName: (context) => SuraDetails(),
+        HadithDetails.routeName: (context) => HadithDetails()
       },
     );
   }
---
+++ lib/model/hadeth_model/hedeth_model.dart
@@ -0,0 +1,6 @@
+class HedathModel {
+  String title;
+  String text;
+
+  HedathModel({required this.title, required this.text});
+}
